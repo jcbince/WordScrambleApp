e4942b7a645fa32861b0103e45b786e236faff8c
Jc Bince <email@jcbince.ca>
2021-07-07 20:42:16 -0700

    adde the UIAlertcontroller, the submit and the submitAction

--- WordScrambleApp/WordScrambleApp/ViewController.swift
+++ WordScrambleApp/WordScrambleApp/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         // Do any additional setup after loading the view.
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt")
         {
@@ -28,7 +30,7 @@ class ViewController: UITableViewController {
         {
             allWords = ["silkworm"]
         }
-        
+        startGame()
     }
     
     func startGame()
@@ -38,6 +40,33 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else {return}
+            self?.submit(answer)
+            
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer:String)
+    {
+        
+    }
 
 
 }
